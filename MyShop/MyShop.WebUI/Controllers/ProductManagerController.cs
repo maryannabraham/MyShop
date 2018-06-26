da461ae38a72233769910fef861b94f82caff90c
Mary Ann <maryannabraham@yahoo.com>
2018-06-26 18:07:00 +0100

    Product Manager pages built and tested

---
+++ MyShop/MyShop.WebUI/Controllers/ProductManagerController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyShop.Core.Models;
+using MyShop.DataAccess.InMemory;
+
+namespace MyShop.WebUI.Controllers
+{
+    public class ProductManagerController : Controller
+    {
+        ProductRepository context;
+
+        public ProductManagerController()
+        {
+            context = new ProductRepository();
+        }
+        // GET: ProductManager
+        public ActionResult Index()
+        {
+            List<Product> products = context.Collection().ToList();
+            return View(products);
+        }
+
+        public ActionResult Create()
+        {
+            Product product = new Product();
+            return View(product);
+        }
+
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(product);
+            }
+            else
+            {
+                context.Insert(product);
+                context.Commit();
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Edit(String Id)
+        {
+            Product product = context.Find(Id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(product);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Product product, String Id)
+        {
+            Product productToEdit = context.Find(Id);
+            if (productToEdit == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View(productToEdit);
+                }
+                else
+                {
+                    productToEdit.Name = product.Name;
+                    productToEdit.Category = product.Category;
+                    productToEdit.Description = product.Description;
+                    productToEdit.Price = product.Price;
+                    productToEdit.Image = product.Image;
+                    context.Commit();
+                    return RedirectToAction("Index");
+                }
+            }
+        }
+
+        public ActionResult Delete(String Id)
+        {
+            Product product = context.Find(Id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(product);
+            }
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+
+        public ActionResult ConfirmDelete(String Id)
+        {
+            Product productToDelete = context.Find(Id);
+            if (productToDelete == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                context.Delete(Id);
+                context.Commit();
+                return RedirectToAction("Index");
+               
+            }
+        }
+    }
+}
